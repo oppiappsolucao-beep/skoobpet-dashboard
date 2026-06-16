c7fbfd67e9f2fc48c41f7d6d4937b4b6468b3229
oppiappsolucao-beep <oppiappsolucao@gmail.com>
2026-06-16 11:40:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="SkoobPet Dashboard",
+    page_icon="🐾",
+    layout="wide"
+)
+
+st.title("🐾 SkoobPet Dashboard")
+st.write("Primeira versão publicada com sucesso.")
+
+st.success("GitHub + Streamlit funcionando.")
